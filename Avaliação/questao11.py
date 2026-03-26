44fbe0e812b20a549d995385c954b1e556164ccb
AnaRegis08 <aajjh2021@gmail.com>
2026-03-26 11:59:27 -0300

    Update input prompt and remove commented code

--- Avaliação/questao11.py
+++ Avaliação/questao11.py
@@ -1,18 +1,13 @@
 alunos = []
 for indice in range(15):
-    entrada = input("N1 N2 N3 N4 Nome")
+    entrada = input("Insira as notas e o nome do aluno(a)(*seguindo esse modelo: N1 N2 N3 N4 Nome): ")
     vetor = entrada.split()
-    # N1 = vetor[0]
-    # n2 = vetor[1]
     [n1, n2, n3, n4, nome] = vetor
     n1 = float(n1)
     n2 = float(n2)
     n3 = float(n3)
     n4 = float(n4)
     media = (n1 + n2 + n3 + n4) / 4
-    # aprovado = media >= 6
-    # reprovado = media < 4
-    # recuperação = 4 <= media < 6
     situacao = ""
     if media >= 6:
         situacao = "aprovado"
